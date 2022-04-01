20bedd2ea96b15320121adbc27e82f9e9d184587
tmenture <thomasoxemail@gmail.com>
2022-03-31 20:47:10 -0400

    added final touches to the website with icons,fonts, and custom colored buttons

--- module_5_starter/taskmaster-starter/assets/js/script.js
+++ module_5_starter/taskmaster-starter/assets/js/script.js
@@ -135,7 +135,7 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   // get the task's position in the list of other li elements
   var index = $(this)
-    .closest(".list-group-item")
+    .closest(".list-group-item") 
     .index();
 
   // update task in array and re-save to localstorage
@@ -160,16 +160,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass(".bottom-trash-drag");
   },
   deactivate: function(event) {
-    
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass(".bottom-trash-drag");
   },
   over: function(event) {
-    
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data
@@ -211,12 +213,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass(".bottom-trash-active");
   },
   over: function(event, ui) {
-  
+    $(".bottom-trash").addClass(".bottom-trash-active");
   },
   out: function(event, ui) {
-    
+    $(".bottom-trash").removeClass(".bottom-trash-active");
   }
 });
 
@@ -237,7 +240,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -275,6 +278,7 @@ var auditTask = function(taskEl) {
   else if(Math.abs(moment().diff(time, "days"))<=2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 // remove all tasks
@@ -289,4 +293,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1800000);
